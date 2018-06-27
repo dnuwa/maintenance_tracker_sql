7de72fabc577de8593e529646d7c804b26a70bc4
daniel nuwa <daniel.nuwa@gmail.com>
2018-06-27 15:37:29 +0300

    updated tests file

--- test_api.py
+++ test_api.py
@@ -20,26 +20,28 @@ class TestUserService(BaseTestCase):
         self.assertEqual(response.status_code, 200)
         #print(result)
         # self.assertIn('issue', result)
-        # self.assertIn('issue_details', result)
-        # self.assertIn('item',result)
-        # self.assertIn('id',result)
+        
 
-    # def test_user_registers_successfuly(self):
-    #     datax = {'email':'daniel.nuwa@yahoo.com', 'password':'53423'}
-    #     response = self.client.post('/api/v1/auth/login',data = json.dumps(datax), content_type='application/json',)
-    #     #print(response)
-    #     self.assertEqual(response.status_code, 201)        
+    def test_user_registers_successfuly(self):
+        userdata = {'email':'daniel.nuwa@yahoo.com', 'password':'53423'}
+        response = self.client.post('/api/v1/auth/signup',data = json.dumps(userdata), content_type='application/json',)
+        print(response)
+        self.assertEqual(response.status_code, 201) 
 
-    # def test_return_all_requests(self):
-    #     response = self.client.get('/api/v1/user/request')
-    #     data = json.loads(response.data)
-    #     self.assertEqual(response.status_code, 200)
-    #     #print(data)
-    #     self.assertIn('item', data)
+    def test_user_login_successfuly(self):
+        response = self.client.post('/api/v1/auth/login')
+        self.assertEqual(response.status_code, 200)      
+
             
-    # def test_access_request_by_id(self):
-    #     response = self.client.get('/api/v1/user/request/1')
-    #     self.assertEqual(response.status_code, 200)  
+    def test_access_request_by_id(self):
+        response = self.client.get('/api/v1/users/requests/1')
+        self.assertEqual(response.status_code, 200) 
+
+    def test_adding_user_request(self):
+        requestData = {'item':'car','issue':'broken dash','issue_details':'radio not working', 'mode':'', 'standing':''} 
+        response = self.client.post('/api/v1/users/requests', data = json.dumps(requestData), content_type='application/json')
+        print(response)
+        self.assertEqual(response.status_code, 201)
 
 if __name__ == '__main__':
     unittest.main()
