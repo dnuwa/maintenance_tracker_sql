da1479290fdc559c27f92218cc0a44b1f6b9c088
daniel nuwa <daniel.nuwa@gmail.com>
2018-06-29 12:39:51 +0300

    added a setup file and updated travis.yml

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='maintenance_tracker_sql',
+      version='1',
+      description='',
+      author='daniel nuwagaba',
+      author_email='daniel.nuwa@gmail.com',
+      url='',
+      packages=find_packages(),
+     )
